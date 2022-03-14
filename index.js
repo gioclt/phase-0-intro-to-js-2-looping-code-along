a6ba410258049a93d6e1ab6731ce6356fa77dfe7
Yobany Caltempa <gioclt26@gmail.com>
2022-03-14 12:50:09 -0400

    commit tree 1fa62676a7eecb431d41d82874a8d47b6b7d82f2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+return newArr
+}
+
+writeCards(names);
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log (num);
+        num--
+    }
+}
+
+
